fc86a510b54125fc8557c9faf57c033bf9d9e387
alexanderw885 <alexanderw885@gmail.com>
2024-04-30 18:28:54 -0700

    Keylogger part of the program now has save/load functions. The next step is to create the program that will use this data to make a new keyboard layout

--- KeyReader/ReaderMain.cpp
+++ KeyReader/ReaderMain.cpp
@@ -2,8 +2,14 @@
 #include <Windows.h>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <sstream>
 #pragma comment(lib, "user32.lib")
 using namespace std;
+
+// Config variables
+const bool debug = FALSE;
+string savePath = "data.txt";
 
 HHOOK hook;
 KBDLLHOOKSTRUCT kbdstruct;
@@ -28,9 +34,45 @@ static void SaveData() {
         save += "\n";
     }
 
-    ofstream fileOut("data.txt");
+    ofstream fileOut(savePath);
     fileOut << save;
     fileOut.close();
+}
+
+static void LoadData() {
+    string save;
+    ifstream fileIn(savePath);
+
+    if (!fileIn) {
+        cout << "No save detected, creating new save" << endl;
+        return;
+    }
+    cout << "Save found, loading data" << endl;
+
+    int i = 0;
+    while (getline(fileIn, save)) {
+
+        stringstream sstream(save);
+        string temp;
+        int j = 0;
+        while (getline(sstream, temp, ',')) {
+            if (i >= 30 || j >= 31) {
+                cout << "save format does not match, exiting program" << endl;
+                exit(1);
+            }
+            presses[i][j] = stoi(temp);
+            j++;
+        }
+        i++;
+    }
+    if (debug) {
+        for (int i = 0; i < 30; i++) {
+            for (int j = 0; j < 31; j++) {
+                cout << presses[i][j] << ",";
+            }
+            cout << endl;
+        }
+    }
 }
 
 static void ProcessChar(int c) {
@@ -60,16 +102,17 @@ static void ProcessChar(int c) {
     if (currChar != -1) {
         if (prevChar != -1) {
             presses[prevChar][currChar]++;
-            cout << prevChar << " " << currChar << endl;
+            if (debug) { cout << prevChar << " " << currChar << endl; }
         }
         presses[currChar][30]++;
-        cout << currChar << " " << 30 << endl;
+        if (debug) { cout << currChar << " " << 30 << endl; }
     }
+    // saves data when space is pressed
     if (c == -65) {
         SaveData();
     }
 
-    cout << "currChar == " << currChar << endl;
+    if (debug) { cout << "currChar == " << currChar << endl; }
     return;
 }
 
@@ -104,7 +147,7 @@ LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
     switch (wParam) {
     case WM_KEYDOWN:
         // Handles Keycodes
-        cout << unicodeCharacter[0] << " key was pressed!\n";
+        if (debug) { cout << unicodeCharacter[0] << " key was pressed!\n"; }
         ProcessChar((int)unicodeCharacter[0]);
         break;
     case WM_KEYUP:
@@ -115,7 +158,13 @@ LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
 }
 
 int main() {
+    LoadData();
     hook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, NULL);
+
+    cout << "Keylogger is now running. Keypresses are saved automatically every time space is pressed.\n"<<
+        "You can find the save file in "<<savePath<<endl;
+    cout << "This program only saves your total number of presses for each key, along with all 2-key sequences.\nIt does not save full words" << endl;
+    cout << "to stop this application, simply close this window." << endl;
 
     MSG msg;
     while (GetMessage(&msg, NULL, NULL, NULL)) { // Message Loop
